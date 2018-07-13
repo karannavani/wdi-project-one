02ef5c2d8d7b19f3d2622af98bdd258fed3a97e0
Karan Navani <kknavani@gmail.com>
2018-07-13 16:44:43 +0100

    fixed start event listener

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  const box = document.querySelector('.box');
+  let gameRunning = false;
   let top = 50;
-  let left = 0;
+  // let left = 0;
   // let right = 0;
   // let bottom = 0;
-  const box = document.querySelector('.box');
 
   $(window).keypress(function(e) {
-    if(e.which === 13) {
-      startGame();
-    } else {
-      console.log('Press Enter');
+    if(!gameRunning) {
+      if(e.which === 13) {
+        startGame();
+      } else {
+        console.log('Press Enter');
+      }
     }
   });
+  // startGame();
 
   function startGame() {
+    gameRunning = true;
     window.setInterval(() => {
       top += 1;
-      left +=1;
-      // right +=1;
-      if (box.style.top < '550px') {
+      if (box.style.top < '570px') {
         box.style.top = (parseInt(top) + top) + 'px';
       } else {
         return;
       }
       console.log(box.style.top);
-      box.style.left = (parseInt(left) + left) + 'px';
-      // box.style.right = (parseInt(right) + right) + 'px';
+
       window.addEventListener('keypress', function() {
-        top = 120;
+        top = 80;
         box.style.top = (parseInt(top) + top) + 'px';
       });
     }, 10);
-
   }
-});
 
 
 
+
+}); //closes DOM listener
 
 //with left working
 // function startGame() {
@@ -60,4 +62,3 @@ window.addEventListener('DOMContentLoaded', () => {
 //   }, 10);
 //
 // }
-// });
