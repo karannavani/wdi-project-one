3a8ae878f1786d43d5f5a4ff116fe45ffffd5f73
Karan Navani <kknavani@gmail.com>
2018-07-13 16:17:51 +0100

    added some test divs for pipes

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-console.log('JS');
 window.addEventListener('DOMContentLoaded', () => {
 
-  let top = 0;
-  let left = 10;
-  let right = -100;
+  let top = 50;
+  let left = 0;
+  // let right = 0;
+  // let bottom = 0;
   const box = document.querySelector('.box');
 
-  window.setInterval(() => {
-    window.addEventListener('keypress', function() {
-      top = 70;
-      box.style.top = (parseInt(top) + top) + 'px';
-    });
-    top += 1;
-    left +=1;
-    right +=1;
-    // box.style.top = (parseInt(top) + top) + 'px';
-    box.style.top = (parseInt(top) + top) + 'px';
-    box.style.left = (parseInt(left) + left) + 'px';
-    box.style.right = (parseInt(right) + right) + 'px';
-  }, 10);
+  $(window).keypress(function(e) {
+    if(e.which === 13) {
+      startGame();
+    } else {
+      console.log('Press Enter');
+    }
+  });
 
-  // document.addEventListener('keypress', (event) => {
-  //   const keyName = event.key;
-  //
-  //   alert('keypress event\n\n' + 'key: ' + keyName);
-  // });
+  function startGame() {
+    window.setInterval(() => {
+      top += 1;
+      left +=1;
+      // right +=1;
+      if (box.style.top < '550px') {
+        box.style.top = (parseInt(top) + top) + 'px';
+      } else {
+        return;
+      }
+      console.log(box.style.top);
+      box.style.left = (parseInt(left) + left) + 'px';
+      // box.style.right = (parseInt(right) + right) + 'px';
+      window.addEventListener('keypress', function() {
+        top = 120;
+        box.style.top = (parseInt(top) + top) + 'px';
+      });
+    }, 10);
 
-
+  }
 });
+
+
+
+
+//with left working
+// function startGame() {
+//   window.setInterval(() => {
+//     top += 1;
+//     left +=1;
+//     // right +=1;
+//     if (box.style.top < '550px') {
+//       box.style.top = (parseInt(top) + top) + 'px';
+//     } else {
+//       return;
+//     }
+//     console.log(box.style.top);
+//     box.style.left = (parseInt(left) + left) + 'px';
+//     // box.style.right = (parseInt(right) + right) + 'px';
+//     window.addEventListener('keypress', function() {
+//       top = 120;
+//       box.style.top = (parseInt(top) + top) + 'px';
+//     });
+//   }, 10);
+//
+// }
+// });
