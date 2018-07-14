a032e19c7b345c76dd177f67087606850e82c215
Karan Navani <kknavani@gmail.com>
2018-07-14 01:17:16 +0100

    added relative positioning to pipes and fixed overflow

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   const box = document.querySelector('.box');
+  const column1 = document.querySelector('.column1');
+  const column2 = document.querySelector('.column2');
   let gameRunning = false;
   let top = 50;
-  // let left = 0;
-  // let right = 0;
+  let left = 250;
+  // let right = 100;
   // let bottom = 0;
 
   $(window).keypress(function(e) {
@@ -12,6 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
       if(e.which === 13) {
         startGame();
         generateColumns();
+        moveColumns();
       } else {
         console.log('Press Enter');
       }
@@ -28,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
       } else {
         return;
       }
-      console.log(box.style.top);
+      // console.log(box.style.top);
 
       window.addEventListener('keypress', function() {
         top = 80;
@@ -38,11 +41,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function generateColumns() {
     window.setInterval(()=> {
-      $('<div>').css({top: 8, left: 700, height: 200, width: 100, backgroundColor: 'green', position: 'fixed' }).appendTo('.game-container');
-      $('<div>').css({bottom: 41, left: 700, height: 200, width: 100, backgroundColor: 'green', position: 'fixed' }).appendTo('.game-container');
+      $('<div>').css({top: 8, left: 700, height: 200, width: 100, backgroundColor: 'green', position: 'fixed' }).appendTo('.columns');
+      $('<div>').css({bottom: 41, left: 700, height: 200, width: 100, backgroundColor: 'green', position: 'fixed' }).appendTo('.columns');
 
     }, 2000);
   }
+
+  function moveColumns() {
+    window.setInterval(() => {
+      left -= 10;
+      column1.style.left = (parseInt(left) + left) + 'px';
+      column2.style.left = (parseInt(left) + left) + 'px';
+      console.log(column1.style.left);
+      if(column1.style.left < `-220px`) {
+        column1.style.backgroundColor = 'black';
+      }
+    },100);
+  }
+
+
 
 
 }); //closes DOM listener
